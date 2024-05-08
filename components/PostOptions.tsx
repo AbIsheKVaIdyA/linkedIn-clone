8e1706e23868f112b658daaa9e9bcb4db58a1b62
Abhishek Vaidya <abhishekvaidya.work@gmail.com>
2024-05-09 01:25:01 +0530

    update part 4

--- components/PostOptions.tsx
+++ components/PostOptions.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { IPostDocument } from "@/mongodb/models/post";
-import { useUser } from "@clerk/nextjs";
+import { SignedIn, useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { MessageCircle, Repeat2, Send, ThumbsUpIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { LikePostRequestBody } from "@/app/api/posts/[post_id]/like/route";
+import { UnlikePostRequestBody } from "@/app/api/posts/[post_id]/unlike/route";
+
+import CommentFeed from "./CommentFeed";
+import CommentForm from "./CommentForm";
 
 function PostOptions({ post }: { post: IPostDocument }) {
   const [isCommentsOpen, setIsCommentsOpen] = useState(false);
@@ -15,6 +23,55 @@ function PostOptions({ post }: { post: IPostDocument }) {
       setLiked(true);
     }
   }, [post, user]);
+
+  const likeOrUnlikePost = async () => {
+    if (!user?.id) {
+      throw new Error("User not authenticated");
+    }
+
+    const originalLiked = liked;
+    const originalLikes = likes;
+
+    const newLikes = liked
+      ? likes?.filter((like) => like !== user.id)
+      : [...(likes ?? []), user.id];
+
+    const body: LikePostRequestBody | UnlikePostRequestBody = {
+      userId: user.id,
+    };
+
+    setLiked(!liked);
+    setLikes(newLikes);
+
+    const response = await fetch(
+      `/api/posts/${post._id}/${liked ? "unlike" : "like"}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
+    if (!response.ok) {
+      setLiked(originalLiked);
+      setLikes(originalLikes);
+      throw new Error("Failed to Like or Unlike post ");
+    }
+
+    const fetchLikesResponse = await fetch(`/api/posts/${post._id}/like`);
+    if (!fetchLikesResponse.ok) {
+      setLiked(originalLiked);
+      setLikes(originalLikes);
+
+      throw new Error("Failed to fetch likes");
+    }
+
+    const newLikeData = await fetchLikesResponse.json();
+
+    setLikes(newLikeData);
+  };
   return (
     <div>
       <div className="flex justify-between p-4">
@@ -37,6 +94,54 @@ function PostOptions({ post }: { post: IPostDocument }) {
           )}
         </div>
       </div>
+
+      <div className="flex p-2 justify-between px-2 border-t">
+        <Button
+          variant="ghost"
+          className="postButton"
+          onClick={likeOrUnlikePost}
+        >
+          <ThumbsUpIcon
+            className={cn("mr-1", liked && "text-[#4881c2] fill-[#4881c2]")}
+          />
+          Like
+        </Button>
+
+        <Button
+          variant="ghost"
+          className="postButton"
+          onClick={() => setIsCommentsOpen(!isCommentsOpen)}
+        >
+          <MessageCircle
+            className={cn(
+              "mr-1",
+              isCommentsOpen && "text-gray-600 fill-gray-600"
+            )}
+          />
+          Comment
+        </Button>
+
+        <Button variant="ghost" className="postButton">
+          <Repeat2 className="mr-1" />
+          Repost
+        </Button>
+
+        <Button variant="ghost" className="postButton">
+          <Send className="mr-1" />
+          Send
+        </Button>
+      </div>
+
+      {isCommentsOpen && (
+        <div className="p-4">
+          <SignedIn>
+            <CommentForm postId={post._id}/>
+          </SignedIn>
+          <CommentFeed post={post} />
+
+          
+        </div>
+      )}
     </div>
   );
 }
