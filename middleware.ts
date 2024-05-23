3316d887dd3c3042f5f9c38726d42d354e1fe681
Abhishek Vaidya <abhishekvaidya.work@gmail.com>
2024-05-23 20:18:08 +0530

    last commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Initialize the Clerk middleware with the publishableKey
+export default clerkMiddleware({
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
